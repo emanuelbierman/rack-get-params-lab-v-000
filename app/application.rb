6e79e5449defe7d52294f722238d06ff5bae981a
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 14:39:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart != nil
+      if @@cart.empty?
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
