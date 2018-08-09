9a45858e1e862278a7c5e19b973fa53b8107d477
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 14:41:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      add_item = req.params[]
+      add_item = req.params["item"]
       if add_item in @@items
         @@cart << add_item
       end
