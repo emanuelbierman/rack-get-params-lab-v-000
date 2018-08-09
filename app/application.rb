804cb29fcc79157c44567880ac2ecd28470e948b
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 14:42:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,8 @@ class Application
       add_item = req.params["item"]
       if @@items.include?(add_item)
         @@cart << add_item
+      else
+        resp.write "We don't have that item"
       end
       resp.write "added #{add_item}"
     else
